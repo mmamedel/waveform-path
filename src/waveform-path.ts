5e303574b1db949b9bd211f0fc7832a28b6617d9
Mateus Mamede Lage <mmamede@gmail.com>
2024-01-12 14:31:14 -0500

    initial changes

--- src/waveform-path.ts
+++ src/waveform-path.ts
@@ -1,15 +1,10 @@
 import type { LinearPathOptions, PolarPathOptions } from "./types";
 
 export function getAudioData(url: string) {
-  // @ts-ignore Backwards compatibility
-  window.AudioContext = window.AudioContext || window.webkitAudioContext;
   const audioContext = new AudioContext();
   return fetch(url)
     .then((response) => response.arrayBuffer())
-    .then((arrayBuffer) => audioContext.decodeAudioData(arrayBuffer))
-    .catch((error) => {
-      console.error(error);
-    });
+    .then((arrayBuffer) => audioContext.decodeAudioData(arrayBuffer));
 }
 
 export function linearPath(
@@ -53,6 +48,285 @@ export function linearPath(
   const audioDataLength = normalizeData.length;
 
   for (let f = 0; f < audioDataLength; f++) {
+    if (f > 0) {
+      const pathlength = path.length;
+      const lastvalue = path.charAt(pathlength - 1);
+      if (lastvalue ==hlength === 0) {
+        path += " M 0 0 ;";
+      } else {
+        path += ";" ";" || pat;
+      }
+    }
+
+    let last_pos_x = -9999;
+    let last_pos_y = -9999;
+
+    for (let i = 0; i < samples; i++) {
+      const positive = type != "bars" ? (i % 2 ? 1 : -1) : 1;
+      let mirror = 1;
+      for (let j = 0; j < fixpathslength; j++) {
+        let k = j;
+        if (j >= pathslength) {
+          k = j - pathslength;
+          mirror = -1;
+        }
+        const currentPath = paths[k];
+        currentPath.minshow = currentPath.minshow ?? 0;
+        currentPath.maxshow = currentPath.maxshow ?? 1;
+        currentPath.normalize = currentPath.normalize ?? false;
+        const normalizeDataValue = currentPath.normalize
+          ? 1
+          : normalizeData[f][i];
+        if (
+          currentPath.minshow <= normalizeData[f][i] &&
+          currentPath.maxshow >= normalizeData[f][i]
+        ) {
+          switch (currentPath.d) {
+            // LineTo Commands
+            case "L": {
+              const pos_x =
+                i * fixWidth + (fixWidth * currentPath.sx) / 100 + left;
+              const pos_y =
+                fixHeight +
+                ((normalizeDataValue * currentPath.sy) / 100) *
+                  (type != "bars" ? height / 2 : height) *
+                  -positive *
+                  mirror;
+
+              const end_pos_x =
+                i * fixWidth + (fixWidth * currentPath.ex) / 100 + left;
+              const end_pos_y =
+                fixHeight +
+                ((normalizeDataValue * currentPath.ey) / 100) *
+                  (type != "bars" ? height / 2 : height) *
+                  -positive *
+                  mirror;
+
+              if (pos_x !== last_pos_x || pos_y !== last_pos_y) {
+                path += `M ${pos_x} ${pos_y} `;
+              }
+
+              path += `L ${end_pos_x} ${end_pos_y} `;
+
+              last_pos_x = end_pos_x;
+              last_pos_y = end_pos_y;
+              break;
+            }
+
+            case "H": {
+              const pos_x =
+                i * fixWidth + (fixWidth * currentPath.sx) / 100 + left;
+              const pos_y =
+                fixHeight +
+                ((normalizeDataValue * currentPath.y) / 100) *
+                  (type != "bars" ? height / 2 : height) *
+                  -positive *
+                  mirror;
+
+              const end_pos_x =
+                i * fixWidth + (fixWidth * currentPath.ex) / 100 + left;
+              const end_pos_y = pos_y;
+
+              if (pos_x !== last_pos_x || pos_y !== last_pos_y) {
+                path += `M ${pos_x} ${pos_y} `;
+              }
+
+              path += `H ${end_pos_x} `;
+
+              last_pos_x = end_pos_x;
+              last_pos_y = end_pos_y;
+              break;
+            }
+
+            case "V": {
+              const pos_x =
+                i * fixWidth + (fixWidth * currentPath.x) / 100 + left;
+              const pos_y =
+                fixHeight +
+                ((normalizeDataValue * currentPath.sy) / 100) *
+                  (type != "bars" ? height / 2 : height) *
+                  -positive *
+                  mirror;
+
+              const end_pos_x = pos_x;
+              const end_pos_y =
+                fixHeight +
+                ((normalizeDataValue * currentPath.ey) / 100) *
+                  (type != "bars" ? height / 2 : height) *
+                  -positive *
+                  mirror;
+
+              if (pos_x !== last_pos_x || pos_y !== last_pos_y) {
+                path += `M ${pos_x} ${pos_y} `;
+              }
+
+              path += `V ${end_pos_y} `;
+
+              last_pos_x = end_pos_x;
+              last_pos_y = end_pos_y;
+              break;
+            }
+
+            // Cubic Bézier Curve Commands
+            case "C": {
+              const pos_x =
+                i * fixWidth + (fixWidth * currentPath.sx) / 100 + left;
+              const pos_y =
+                fixHeight - ((fixHeight * currentPath.sy) / 100) * positive;
+
+              const center_pos_x =
+                i * fixWidth + (fixWidth * currentPath.x) / 100 + left;
+              const center_pos_y =
+                fixHeight +
+                ((normalizeDataValue * currentPath.y) / 100) *
+                  (type != "bars" ? height : height * 2) *
+                  -positive *
+                  mirror;
+
+              const end_pos_x =
+                i * fixWidth + (fixWidth * currentPath.ex) / 100 + left;
+              const end_pos_y =
+                fixHeight - ((fixHeight * currentPath.ey) / 100) * positive;
+
+              if (pos_x !== last_pos_x || pos_y !== last_pos_y) {
+                path += `M ${pos_x} ${pos_y} `;
+              }
+
+              path += `C ${pos_x} ${pos_y} ${center_pos_x} ${center_pos_y} ${end_pos_x} ${end_pos_y} `;
+
+              last_pos_x = end_pos_x;
+              last_pos_y = end_pos_y;
+              break;
+            }
+
+            // Quadratic Bézier Curve Commands
+            case "Q": {
+              const pos_x =
+                i * fixWidth + (fixWidth * currentPath.sx) / 100 + left;
+              const pos_y =
+                fixHeight +
+                ((normalizeDataValue * currentPath.sy) / 100) *
+                  (type != "bars" ? height / 2 : height) *
+                  -positive *
+                  mirror;
+
+              const center_pos_x =
+                i * fixWidth + (fixWidth * currentPath.x) / 100 + left;
+              const center_pos_y =
+                fixHeight +
+                ((normalizeDataValue * currentPath.y) / 100) *
+                  (type != "bars" ? height : height * 2) *
+                  -positive *
+                  mirror;
+
+              const end_pos_x =
+                i * fixWidth + (fixWidth * currentPath.ex) / 100 + left;
+              const end_pos_y =
+                fixHeight +
+                ((normalizeDataValue * currentPath.ey) / 100) *
+                  (type != "bars" ? height / 2 : height) *
+                  -positive *
+                  mirror;
+
+              if (pos_x !== last_pos_x || pos_y !== last_pos_y) {
+                path += `M ${pos_x} ${pos_y} `;
+              }
+
+              path += `Q ${center_pos_x} ${center_pos_y} ${end_pos_x} ${end_pos_y} `;
+
+              last_pos_x = end_pos_x;
+              last_pos_y = end_pos_y;
+              break;
+            }
+
+            // Elliptical Arc Curve Commands
+            case "A": {
+              const pos_x =
+                i * fixWidth + (fixWidth * currentPath.sx) / 100 + left;
+              const pos_y =
+                fixHeight +
+                ((normalizeDataValue * currentPath.sy) / 100) *
+                  (type != "bars" ? height / 2 : height) *
+                  -positive *
+                  mirror;
+
+              const end_pos_x =
+                i * fixWidth + (fixWidth * currentPath.ex) / 100 + left;
+              const end_pos_y =
+                fixHeight +
+                ((normalizeDataValue * currentPath.ey) / 100) *
+                  (type != "bars" ? height / 2 : height) *
+                  -positive *
+                  mirror;
+
+              if (pos_x !== last_pos_x || pos_y !== last_pos_y) {
+                path += `M ${pos_x} ${pos_y} `;
+              }
+              const rx = (currentPath.rx * fixWidth) / 100;
+              const ry = (currentPath.ry * fixWidth) / 100;
+              let sweep = currentPath.sweep;
+              if (positive == -1) {
+                if (sweep == 1) {
+                  sweep = 0;
+                } else {
+                  sweep = 1;
+                }
+              }
+              if (mirror == -1) {
+                if (sweep == 1) {
+                  sweep = 0;
+                } else {
+                  sweep = 1;
+                }
+              }
+              path += `A ${rx} ${ry} ${currentPath.angle} ${currentPath.arc} ${sweep} ${end_pos_x} ${end_pos_y} `;
+
+              last_pos_x = end_pos_x;
+              last_pos_y = end_pos_y;
+              break;
+            }
+
+            // ClosePath Commands
+            case "Z":
+              path += "Z ";
+              break;
+
+            default:
+              break;
+          }
+        }
+      }
+    }
+  }
+  return path;
+}
+
+export function linearPathFromAudioData(
+  normalizeData: number[][],
+  options: LinearPathOptions
+) {
+  const {
+    samples = normalizeData.length,
+    height = 100,
+    width = 800,
+    top = 0,
+    left = 0,
+    type = "steps",
+    paths = [
+      { d: "Q", sx: 0, sy: 0, x: 50, y: 100, ex: 100, ey: 0 },
+    ] as LinearPathOptions["paths"],
+    start = 0,
+    end = normalizeData.length,
+  } = options;
+
+  let path = ``;
+
+  const fixHeight = type != "bars" ? (height + top * 2) / 2 : height + top;
+  const fixWidth = width / samples;
+  const pathslength = paths.length;
+  const fixpathslength = type == "mirror" ? pathslength * 2 : pathslength;
+
+  for (let f = start; f < end; f++) {
     if (f > 0) {
       const pathlength = path.length;
       const lastvalue = path.charAt(pathlength - 1);
@@ -748,6 +1022,29 @@ export function getFilterData(framesData: Float32Array[], samples: number) {
   return filteredData;
 }
 
+export function getFilterDataWithOffset(
+  framesData: Float32Array[],
+  samples: number,
+  start = 0,
+  end = framesData.length
+) {
+  const filteredData = [] as number[][];
+  for (let f = start; f < end; f++) {
+    const blockSize = Math.floor(framesData[f].length / samples); // the number of samples in each subdivision
+    const filteredDataBlock = [] as number[];
+    for (let i = 0; i < samples; i++) {
+      let blockStart = blockSize * i; // the location of the first sample in the block
+      let sum = 0;
+      for (let j = 0; j < blockSize; j++) {
+        sum = sum + Math.abs(framesData[f][blockStart + j]); // find the sum of all the samples in the block
+      }
+      filteredDataBlock.push(sum / blockSize); // divide the sum by the block size to get the average
+    }
+    filteredData.push(filteredDataBlock);
+  }
+  return filteredData;
+}
+
 export function getNormalizeData(filteredData: number[][]) {
   const multipliers = [] as number[];
   const filteredDataLength = filteredData.length;
@@ -759,6 +1056,29 @@ export function getNormalizeData(filteredData: number[][]) {
   const normalizeData = [] as number[][];
   for (let i = 0; i < filteredDataLength; i++) {
     const normalizeDataBlock = filteredData[i].map((n) => n * maxMultiplier);
+    normalizeData.push(normalizeDataBlock);
+  }
+  return normalizeData;
+}
+
+export function getMaxMultipler(framesData: Float32Array[]) {
+  const multipliers = [] as number[];
+  const filteredDataLength = framesData.length;
+  for (let i = 0; i < filteredDataLength; i++) {
+    const multiplier = getMax(framesData[i]);
+    multipliers.push(multiplier);
+  }
+  return getMax(multipliers);
+}
+
+export function getNormalizeDataFromMaxMultipler(
+  framesData: Float32Array[],
+  maxMultiplier: number
+) {
+  const filteredDataLength = framesData.length;
+  const normalizeData = [] as Float32Array[];
+  for (let i = 0; i < filteredDataLength; i++) {
+    const normalizeDataBlock = framesData[i].map((n) => n * maxMultiplier);
     normalizeData.push(normalizeDataBlock);
   }
   return normalizeData;
